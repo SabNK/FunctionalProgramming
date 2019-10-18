ea5d8f5684ff901a7d504ed5f707e4354ced861e
SabNK <sabnk@optictelecom.ru>
2019-10-18 16:39:32 +0300

    superiterable

---
+++ src/functional/SuperIterable.java
@@ -0,0 +1,26 @@
+package functional;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+
+public class SuperIterable<E> implements Iterable<E> {
+	private Iterable<E> self;
+
+	public SuperIterable(Iterable self) {
+		this.self = self;
+	}
+
+	@Override
+	public Iterator<E> iterator() {
+		return self.iterator();
+	}
+	
+	public static void main(String[] args) {
+		SuperIterable<String> strings = new SuperIterable<>(Arrays.asList("LightCoral", "pink",
+				"orange", "gold", "plum", "Blue", "limegreen"));
+		for (String s: strings) {
+			System.out.println(s);
+		}
+	}	
+}
